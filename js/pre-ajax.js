f47cfe029a3cbfff27882da3d13ea66f86f7affe
Xavier Salazar <salazar.xavier26@gmail.com>
2018-07-09 10:03:57 -0500

    ajax warmup

---
+++ js/pre-ajax.js
@@ -0,0 +1,29 @@
+'use strict';
+
+$(document).ready(function () {
+
+    var people = [
+        {
+            name: 'Fred',
+            favColor: 'red'
+        },
+        {
+            name: 'Sally',
+            favColor: 'blue'
+        },
+        {
+            name: 'Tom',
+            favColor: 'yellow'
+        }
+    ];
+
+    $('#names-list').append('<ul></ul>');
+    $('#add-names-btn').click(function () {
+        people.forEach(function (person) {
+            $('#names-list ul').append('<li>' + person.name + '</li>');
+        });
+        $('li').each(function (index) {
+            $(this).css('color', people[index].favColor);
+        })
+    });
+});
