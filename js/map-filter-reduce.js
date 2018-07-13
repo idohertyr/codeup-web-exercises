82e56ad0aa48a83c17bbe069157ef6a094fb7177
Xavier Salazar <salazar.xavier26@gmail.com>
2018-07-13 12:11:20 -0500

    map-filter-reduce exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,41 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+let moreThanThree = users.filter(user => user.languages.length >= 3);
+let emails = users.map(user => user.email);
+let userObj = users.reduce((accumulation, curObj) => {
+   accumulation[curObj.id] = curObj;
+   return accumulation;
+}, {});
+
+console.log(userObj);
